dc8cdc3f71063485aa5c938fcdef02e47580afd6
Okek <87618530+skuyykek69@users.noreply.github.com>
2021-08-28 17:22:14 +0700

    Nyoba doang tod

--- config.py
+++ config.py
@@ -32,6 +32,6 @@ BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False)) # just fill
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . -").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
